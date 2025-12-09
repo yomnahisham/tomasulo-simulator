cfaaecf4eb0f9b3d36b79489c6d71e3ffc2ed0ae
jacquesjeanelia <159173424+jacquesjeanelia@users.noreply.github.com>
2025-12-09 22:08:53 +0200

    test3

---
+++ testcases/test3.s
@@ -0,0 +1,21 @@
+# Test Case: Branch Taken with Speculative Instruction Flushing
+# This tests ROB flushing when branch prediction is wrong
+
+# Initialize registers
+LOAD R1, 0(R0)      # R1 = memory[0] = 5
+LOAD R2, 4(R0)      # R2 = memory[4] = 5
+
+# Branch that will be taken (R1 == R2)
+CALL TARGET # Jump to TARGET unconditionally
+
+# Speculative instructions (should be FLUSHED)
+ADD R3, R1, R2      # R3 = R1 + R2 (SHOULD BE FLUSHED)
+MUL R4, R3, R1      # R4 = R3 * R1 (SHOULD BE FLUSHED)
+SUB R5, R4, R2      # R5 = R4 - R2 (SHOULD BE FLUSHED)
+STORE R5, 8(R0)     # memory[8] = R5 (SHOULD BE FLUSHED)
+
+TARGET:
+# Correct path after branch taken
+ADD R6, R1, R2      # R6 = R1 + R2 = 10 (SHOULD EXECUTE)
+MUL R7, R6, R1      # R7 = R6 * R1 = 50 (SHOULD EXECUTE)
+STORE R7, 12(R0)    # memory[12] = R7 (SHOULD EXECUTE)
